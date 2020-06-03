adc597e9e374ae54b8bacf58c746ea1bf0e558f8
ne <naesup@gmail.com>
2020-06-03 18:26:11 +0700

    fix eslint format

--- src/commands/fortune.js
+++ src/commands/fortune.js
@@ -1,69 +1,72 @@
-import { exampleCommand, beep, withStar } from "../utilities/string";
-import { sendError } from "../utilities/message";
+import { exampleCommand, beep, withStar } from '../utilities/string'
+import { sendError } from '../utilities/message'
 
-import { getOneCardData, getMajorCards } from "../services/tarot";
-import fortuneInfo from "../templates/fortuneInfo";
+import { getOneCardData, getMajorCards } from '../services/tarot'
+import fortuneInfo from '../templates/fortuneInfo'
 
 const error = [
-  beep("You may give bubeep a wrong type."),
+  beep('You may give bubeep a wrong type.'),
   exampleCommand('"tarot normal" or "tarot single"'),
 ]
 
 const randomCardNumber = () => Math.floor(Math.random() * 22)
 const randomReverse = () => Math.floor(Math.random() >= 0.5)
 
-const getNotDuplicatedNumberList = (amount) => {
-  const NumberList = []
-  do{
-    const _cardNumber = randomCardNumber()
-    if(!NumberList.includes(_cardNumber))
-      { NumberList.push(_cardNumber) }
-  }while(NumberList.length < amount)
-  return NumberList
+const getNotDuplicatedNumberList = amount => {
+  const numberList = []
+  do {
+    const cardNumber = randomCardNumber()
+    if (!numberList.includes(cardNumber)) {
+      numberList.push(cardNumber)
+    }
+  } while (numberList.length < amount)
+  return numberList
 }
 
-const drawManyCard = async (amount) => {
-  const _numberList = getNotDuplicatedNumberList(amount)
-  const _majorCards = await getMajorCards()
-  const _cardDataList = _numberList.map((number)=> {
-    return { card: _majorCards[number] ,isReversed: randomReverse() }})
-  return Object.values(_cardDataList);
+const drawManyCard = async amount => {
+  const numberList = getNotDuplicatedNumberList(amount)
+  const majorCards = await getMajorCards()
+  const cardDataList = numberList.map(number => ({
+    card: majorCards[number],
+    isReversed: randomReverse(),
+  }))
+  return Object.values(cardDataList)
 }
 
 const drawCard = async () => {
   const card = await getOneCardData(randomCardNumber())
   const isReversed = randomReverse()
-  return { card, isReversed };
+  return { card, isReversed }
 }
 
-const execute = async (message, param = "normal") => {
-  message.channel.send("let me see. hmm...")
+const execute = async (message, param = 'normal') => {
+  message.channel.send('let me see. hmm...')
   try {
-    let data;
+    let data
     switch (param) {
-      case "normal":
-        data = await drawManyCard(3)
-        break;
-      case "single":
-        data = [await drawCard()]
-        break;
-      default:
-        data = null
+    case 'normal':
+      data = await drawManyCard(3)
+      break
+    case 'single':
+      data = [await drawCard()]
+      break
+    default:
+      data = null
     }
 
     if (data) {
-      data.forEach((item) => message.channel.send(fortuneInfo(item)))
+      data.forEach(item => message.channel.send(fortuneInfo(item)))
     } else {
-      message.channel.send("no card draw.")
+      message.channel.send('no card draw.')
     }
   } catch (e) {
-    sendError(e, message, `${withStar("BEEPBOOP")} Something Error O_o`)
+    sendError(e, message, `${withStar('BEEPBOOP')} Something Error O_o`)
   }
 }
 
 export default {
-  name: "fortune",
-  desc: "Let me read your mind then represent it as a card. ",
+  name: 'fortune',
+  desc: 'Let me read your mind then represent it as a card. ',
   cooldown: 5,
   param: 1, // 0: no param, 1: optional, 2: required
   execute,
--- src/services/tarot.js
+++ src/services/tarot.js
@@ -1,15 +1,15 @@
-import axios from "./axios";
+import axios from './axios'
 
 export const getMajorCards = async () => {
   const { data } = await axios.get(
-    "https://rws-cards-api.herokuapp.com/api/v1/cards/search?type=major"
+    'https://rws-cards-api.herokuapp.com/api/v1/cards/search?type=major',
   )
   return data.cards
 }
 
 export const getOneCardData = async (cardNumber = 0) => {
   const url = `https://rws-cards-api.herokuapp.com/api/v1/cards/ar${
-    cardNumber < 10 ? 0 : ""
+    cardNumber < 10 ? 0 : ''
   }${cardNumber}`
   const { data } = await axios.get(url)
   return data.card
