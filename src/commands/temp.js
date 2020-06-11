3862a05cdbd34ca88c9f6fc139053302d4a11b97
Nelynx09 <naesup@gmail.com>
2020-06-11 23:57:43 +0700

    edit follow review

--- src/commands/temp.js
+++ src/commands/temp.js
@@ -1,52 +1,40 @@
-import { exampleCommand, beep } from '../utilities/string'
+import { beep } from '../utilities/string'
 import { getRandomInt } from '../utilities/number'
 
 // site for get TextToSpeech
 // https://soundoftext.com/#learn
 
-const voiceUrls = {
-  yourTemp:
-    'https://soundoftext.nyc3.digitaloceanspaces.com/8a2999e0-ab35-11ea-a916-9d0df1ae6773.mp3',
-  thirtySixDot:
-    'https://soundoftext.nyc3.digitaloceanspaces.com/ae4e87e0-ab35-11ea-a916-9d0df1ae6773.mp3',
+const VOICE_URL = 'https://soundoftext.nyc3.digitaloceanspaces.com'
+const VOICE_FILE_NAME = {
+  yourTemp: '8a2999e0-ab35-11ea-a916-9d0df1ae6773.mp3',
+  thirtySixDot: 'ae4e87e0-ab35-11ea-a916-9d0df1ae6773.mp3',
+  1: 'd26f9740-cb46-11e7-a240-b9639a1828db.mp3',
+  2: 'db4566b0-cb46-11e7-a240-b9639a1828db.mp3',
+  3: 'e75fb2c0-cb46-11e7-a240-b9639a1828db.mp3',
+  4: 'f00c7660-cb46-11e7-a240-b9639a1828db.mp3',
+  5: 'f82d4db0-cb46-11e7-a240-b9639a1828db.mp3',
+  6: '00c06ed0-cb46-11e7-a240-b9639a1828db.mp3',
+  7: '06ef3a20-cb46-11e7-a240-b9639a1828db.mp3',
+  8: '0be75a30-cb46-11e7-a240-b9639a1828db.mp3',
+  9: '12758390-cb46-11e7-a240-b9639a1828db.mp3',
 }
-
-const voiceNumberUrls = {
-  1: 'd26f9740',
-  2: 'db4566b0',
-  3: 'e75fb2c0',
-  4: 'f00c7660',
-  5: 'f82d4db0',
-  6: '00c06ed0',
-  7: '06ef3a20',
-  8: '0be75a30',
-  9: '12758390',
-}
+const getVoiceLine = name => `${VOICE_URL}/${VOICE_FILE_NAME[name]}`
 
 const errorNoConnection = [
   beep('You need to join voice channel'),
 ]
 
-const errorParam = [
-  beep('You don\'need to tell bubeep much.'),
-  exampleCommand('temp'),
-]
-
-const execute = async (message, param) => {
-  if (param != null) {
-    message.channel.send(errorParam)
-  } else if (!message.member.voice.channel) {
+const execute = async message => {
+  if (!message.member.voice.channel) {
     message.channel.send(errorNoConnection)
   } else {
     const connection = await message.member.voice.channel.join()
 
-    let dispatcher = connection.play(voiceUrls.yourTemp)
+    let dispatcher = connection.play(getVoiceLine('yourTemp'))
     dispatcher.on('finish', () => {
-      dispatcher = connection.play(voiceUrls.thirtySixDot)
+      dispatcher = connection.play(getVoiceLine('thirtySixDot'))
       dispatcher.on('finish', () => {
-        const voiceLineNumberUrl = `https://soundoftext.nyc3.digitaloceanspaces.com/${
-          voiceNumberUrls[getRandomInt(9, 1)]
-        }-cb46-11e7-a240-b9639a1828db.mp3`
+        const voiceLineNumberUrl = getVoiceLine(getRandomInt(9, 1))
         connection.play(voiceLineNumberUrl)
       })
     })
@@ -54,8 +42,8 @@ const execute = async (message, param) => {
 }
 
 export default {
-  name: 'temp',
-  desc: 'measuring your temperature',
+  name: 'fever',
+  desc: 'measuring your body temperature',
   param: 0, // 0: no param, 1: optional, 2: required
   execute,
 }
