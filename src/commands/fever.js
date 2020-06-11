20eb60edc915443a3ae6534547a99f22024e4da3
Nelynx09 <naesup@gmail.com>
2020-06-12 00:22:17 +0700

    change getVoiceLine url

--- src/commands/fever.js
+++ src/commands/fever.js
@@ -1,10 +1,7 @@
 import { beep } from '../utilities/string'
 import { getRandomInt } from '../utilities/number'
 
-const VOICE_URL_START = 'https://translate.google.com.vn/translate_tts?ie=UTF-8&q='
-const VOICE_URL_END = '&tl=th&client=tw-ob'
-
-const getVoiceLine = word => `${VOICE_URL_START}${word}${VOICE_URL_END}`
+const getVoiceLine = word => `https://translate.google.com.vn/translate_tts?ie=UTF-8&q=${word}&tl=th&client=tw-ob`
 
 const errorNoConnection = [
   beep('You need to join voice channel'),
