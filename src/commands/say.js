459a713cb11a21cc8b205ca2de2485be3192b648
gigigimay <mo.maytiya_st@tni.ac.th>
2020-06-13 03:58:49 +0700

    add sakod and say

---
+++ src/commands/say.js
@@ -0,0 +1,29 @@
+import { beep, exampleCommand } from '../utilities/string'
+import { getVoiceLine } from '../services/tts'
+
+const errors = {
+  noConnection: beep('You need to join a voice channel'),
+  emptyParams: [
+    beep('Please tell bubeep what to *say*.'),
+    exampleCommand('echo hello'),
+  ],
+}
+
+const execute = async (message, param) => {
+  if (!param) {
+    return message.channel.send(errors.noConnection)
+  }
+  if (!message.member.voice.channel) {
+    return message.channel.send(errors.noConnection)
+  }
+  const connection = await message.member.voice.channel.join()
+  const voice = getVoiceLine(`${param.replace(/\s/g, '%20')}`)
+  connection.play(voice)
+}
+
+export default {
+  name: 'say',
+  desc: 'I say.',
+  param: 0, // 0: no param, 1: optional, 2: required
+  execute,
+}
