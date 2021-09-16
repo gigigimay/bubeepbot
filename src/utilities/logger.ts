b78b6a1231e11b3b1ca8174e19b3f59b6e729653
gigigimay <mo.maytiya_st@tni.ac.th>
2021-09-17 00:18:05 +0700

    add winston + logger function

---
+++ src/utilities/logger.ts
@@ -0,0 +1,17 @@
+import { createLogger as createWinstonLogger, transports, format, Logger } from 'winston'
+
+export const createLogger = (label = '*'): Logger =>
+  createWinstonLogger({
+    format: format.combine(
+      format.colorize(),
+      format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+      format.label({ label }),
+      format.printf(
+        (lgr) =>
+          `${lgr.timestamp} - ${lgr.level} - [${lgr.label}]: ${lgr.message}`,
+      ),
+    ),
+    transports: [
+      new transports.Console({ level: 'http' }),
+    ],
+  })
