d48c5dbec68c88969aacb4a1221537b52454803d
gigigimay <mo.maytiya_st@tni.ac.th>
2021-09-17 02:27:43 +0700

    add refreshGuildCommands func

---
+++ src/helper/guild.ts
@@ -0,0 +1,27 @@
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+import { SlashCommandInfo } from '../types'
+import { createLogger } from '../utilities/logger'
+
+const logger = createLogger('helper/guild.ts')
+
+const BOT_TOKEN = process.env.BOT_TOKEN ?? ''
+const CLIENT_ID = process.env.CLIENT_ID ?? ''
+
+const rest = new REST({ version: '9' }).setToken(BOT_TOKEN)
+
+export const refreshGuildCommands = async (
+  guildId: string,
+  commandConfig: SlashCommandInfo[],
+): Promise<void> => {
+  try {
+    logger.info(`Started refreshing application (/) commands for guildId ${guildId}.`)
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, guildId),
+      { body: commandConfig },
+    )
+    logger.info(`Successfully reloaded application (/) commands for guildId ${guildId}.`)
+  } catch (error) {
+    logger.error(error)
+  }
+}
