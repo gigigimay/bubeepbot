faba6717d7786126a914ffe65b57cfdfd9469635
gigigimay <mo.maytiya_st@tni.ac.th>
2021-09-17 03:48:53 +0700

    WIP try to connecting voice + init some interactions

--- src/handler/interaction.ts
+++ src/handler/interaction.ts
@@ -5,12 +5,16 @@ const logger = createLogger('handler/interaction.ts')
 
 export const handleCommandInteraction = (interaction: CommandInteraction): void => {
   try {
-    // TODO: handle more commands
+    // TODO: handle more commands in a more structured way
     if (interaction.commandName === 'ping') {
       interaction.reply('Pong!')
     }
+    if (interaction.commandName === 'beep') {
+      interaction.reply('Beep!')
+    }
   } catch (e) {
     logger.error('logger')
-    interaction.reply(e.message)
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    interaction.reply((e as any).message)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Client, Intents } from 'discord.js'
+import { Client, Intents, Message } from 'discord.js'
+import { joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice'
 import { parseCommand } from './utilities/string'
-import handler from './handler'
-import { init as initCommands } from './helper/command'
+// import handler from './handler'
+// import { init as initCommands } from './helper/command'
 import { refreshGuildCommands } from './helper/guild'
 import { createLogger } from './utilities/logger'
 import { handleCommandInteraction } from './handler/interaction'
@@ -9,13 +10,16 @@ import { handleCommandInteraction } from './handler/interaction'
 const logger = createLogger('index.ts')
 
 // FIXME
-const slashCommands = [{
-  name: 'ping',
-  description: 'Replies with Pong!',
-}]
-
-// FIXME: make a message command for this
-refreshGuildCommands('707283564655280230', slashCommands)
+const slashCommands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'beep',
+    description: 'Beep beep 01!',
+  },
+]
 
 const client = new Client({
   intents: [
@@ -24,7 +28,7 @@ const client = new Client({
   ],
 })
 
-initCommands()
+// initCommands()
 
 // trigger once after logging in
 client.once('ready', () => {
@@ -37,15 +41,77 @@ client.on('interactionCreate', (interaction) => {
 })
 
 // // subscribe to message event
-client.on('messageCreate', (message) => {
-  // FIXME
-  console.log('message >>>', message.guildId)
+client.on('messageCreate', async (message) => {
+  console.log('message >>>', message.member?.voice.channelId)
+  console.log('message >>>', message.member?.fetch())
 
-  if (message.author.bot) return
-  const parsed = parseCommand(message.content)
-  if (parsed) {
-    handler(parsed, message)
+  // FIXME move to message command
+  if (message.content === 'refresh') {
+    if (message.guildId) {
+      await refreshGuildCommands(message.guildId, slashCommands)
+      message.reply('refreshing command done!')
+    }
   }
+
+  // FIXME buggy: cannot change channel
+  if (message.content === 'join') {
+    const member = await message.member?.fetch()
+    const channelId = member?.voice.channelId
+    const guildId = message.guildId
+    const adapterCreator = message.guild?.voiceAdapterCreator
+    console.log('joinVoiceConfig >>>', {
+      member,
+      channelId,
+      guildId,
+      adapterCreator,
+    })
+    if (channelId && guildId && adapterCreator) {
+      logger.info('WILL JOIN~')
+      const connection = joinVoiceChannel({
+        channelId,
+        guildId,
+        adapterCreator,
+      })
+      // setTimeout(() => {
+      //   logger.info('Leaving voice channel.. (A)')
+      //   connection.destroy()
+      // }, 5000)
+      // connection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
+      //   logger.info('Voice connection is in the Ready state!')
+      //   setTimeout(() => {
+      //     logger.info('Leaving voice channel.. (B)')
+      //     connection.destroy()
+      //   }, 3000)
+      // })
+      connection.on(VoiceConnectionStatus.Disconnected, () => {
+        logger.info('VoiceConnectionStatus.Disconnected')
+      })
+      connection.on(VoiceConnectionStatus.Signalling, () => {
+        logger.info('VoiceConnectionStatus.Signalling')
+      })
+      connection.on(VoiceConnectionStatus.Connecting, () => {
+        logger.info('VoiceConnectionStatus.Connecting')
+      })
+      connection.on(VoiceConnectionStatus.Destroyed, () => {
+        logger.info('VoiceConnectionStatus.Destroyed')
+      })
+      connection.on('stateChange', (oldState, newState) => {
+        console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+      });
+      connection.on('error', (err) => {
+        logger.error(`connection error`, err)
+      });
+
+    } else {
+      logger.info('NOT IN A VOICE CHANNEL!')
+    }
+  }
+
+  // if (message.author.bot) return
+  // const parsed = parseCommand(message.content)
+  // if (parsed) {
+  //   handler(parsed, message)
+  // }
 })
 
 client.on('error', (error) => {
