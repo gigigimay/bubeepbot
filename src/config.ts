8c6e81f8dea38f20c7407e36a6d6ce927a8d7783
gigigimay <mo.maytiya_st@tni.ac.th>
2022-04-22 21:37:09 +0700

    revert config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  prefix: process.env.COMMAND_PREFIX || 'n!',
+  prefix: process.env.COMMAND_PREFIX || 'b!',
   suffix: process.env.COMMAND_SUFFIX || '',
   voiceChannelTimeout: 300
 }
