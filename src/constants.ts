cc5153416a1b1a6407475ccc92a1dfb55aee96af
gigigimay <mo.maytiya_st@tni.ac.th>
2022-04-23 03:02:30 +0700

    add emoji constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
 export const BUBEEP_AVATAR = 'https://cdn.discordapp.com/avatars/707274116205641778/a73128d7a7eb584b35995f64afcb814f.png'
 export const INVITE_URL = 'https://discord.com/oauth2/authorize?client_id=707274116205641778&scope=bot'
+
+export const EMOJIS = {
+  PLAY: { id: '967149381868466276', message: '<:play_white:967149381868466276>' },
+  PLEASEEEEE: { id: '967134652278132827', message: '<:pleaseeeee:967134652278132827>' },
+}
